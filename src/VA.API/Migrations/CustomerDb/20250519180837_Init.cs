8361ee38fcd48fb8f44927856c76535c7927e552
Amit P Naik <amit.naik8103@gmail.com>
2025-05-20 00:06:48 +0530

    feat: adding migration

---
+++ src/VA.API/Migrations/CustomerDb/20250519180837_Init.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace VA.API.Migrations.CustomerDb
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    CustomerCode = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CustomerName = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "Id", "CustomerCode", "CustomerName" },
+                values: new object[,]
+                {
+                    { new Guid("0c30022b-8617-47ec-8e2d-6f327f507084"), "CUST-002", "Jane Smith" },
+                    { new Guid("e455f48f-35d1-4fa4-aaf1-4f7fcf5da22a"), "CUST-001", "John Doe" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
