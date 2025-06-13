dfb8f9224809f8d19ae12d5f586236522d9daced
Bharat <bharat.mane@gmail.com>
2025-06-13 20:45:39 +0530

    chore: demo pr

--- src/Yourdrs.CrossCutting/Exceptions/Handler/CustomExceptionHandler.cs
+++ src/Yourdrs.CrossCutting/Exceptions/Handler/CustomExceptionHandler.cs
@@ -9,7 +9,7 @@ public class CustomExceptionHandler
     (ILogger<CustomExceptionHandler> logger)
     : IExceptionHandler
 {
-    public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
     {
         logger.LogError(
             "Error Message: {exceptionMessage}, Time of occurrence {time}",
@@ -21,31 +21,31 @@ public class CustomExceptionHandler
             (
                 exception.Message,
                 exception.GetType().Name,
-                context.Response.StatusCode = StatusCodes.Status500InternalServerError
+                httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError
             ),
             ValidationException =>
             (
                 exception.Message,
                 exception.GetType().Name,
-                context.Response.StatusCode = StatusCodes.Status400BadRequest
+                httpContext.Response.StatusCode = StatusCodes.Status400BadRequest
             ),
             BadRequestException =>
             (
                 exception.Message,
                 exception.GetType().Name,
-                context.Response.StatusCode = StatusCodes.Status400BadRequest
+                httpContext.Response.StatusCode = StatusCodes.Status400BadRequest
             ),
             NotFoundException =>
             (
                 exception.Message,
                 exception.GetType().Name,
-                context.Response.StatusCode = StatusCodes.Status404NotFound
+                httpContext.Response.StatusCode = StatusCodes.Status404NotFound
             ),
             _ =>
             (
                 exception.Message,
                 exception.GetType().Name,
-                context.Response.StatusCode = StatusCodes.Status500InternalServerError
+                httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError
             )
         };
 
@@ -54,17 +54,17 @@ public class CustomExceptionHandler
             Title = details.Title,
             Detail = details.Detail,
             Status = details.StatusCode,
-            Instance = context.Request.Path
+            Instance = httpContext.Request.Path
         };
 
-        problemDetails.Extensions.Add("traceId", context.TraceIdentifier);
+        problemDetails.Extensions.Add("traceId", httpContext.TraceIdentifier);
 
         if (exception is ValidationException validationException)
         {
             problemDetails.Extensions.Add("ValidationErrors", validationException.Errors);
         }
 
-        await context.Response.WriteAsJsonAsync(problemDetails, cancellationToken: cancellationToken);
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken: cancellationToken);
         return true;
     }
 }
